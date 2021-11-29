b982ee5091c5478ddad326618fa8662561410a4e
Tipakoder <staryliss.nikita.2004@gmail.com>
2021-11-29 09:50:10 +0500

    Добавлена возможность запуска только http сервера.

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ if(process.argv.indexOf("--cluster") !== -1) {
         for(let i = 0; i < cpuLength; i++) {
             let workerName = "worker-http";
 
-            if(i > (cpuLength / 2))
+            // Start socket servers
+            if(
+                i >= (cpuLength / 2) &&
+                process.argv.indexOf("--with-socket") !== -1
+            )
                 workerName = "worker-socket";
 
             // Launch
@@ -33,7 +37,7 @@ if(process.argv.indexOf("--cluster") !== -1) {
         require(`./src/${process.env.WORKER_NAME}`);
     }
 } else {
-    console.log(colors.rainbow(`> ORENBURG EDUCATION FORUM (cpus: ${cpuLength})`));
+    console.log(colors.rainbow(`> EDUCATION PLATFORM (cpus: ${cpuLength})`));
 
     require(`./src/worker-http`);
     require(`./src/worker-socket`);
--- src/worker-socket.js
+++ src/worker-socket.js
@@ -1,1 +1,4 @@
+const colors = require("colors");
+
 require("./env");
+console.log(colors.green("> SOCKET SERVER COPY STARTED (FAKE)"), colors.bgYellow(`(pid: ${process.pid})`));
