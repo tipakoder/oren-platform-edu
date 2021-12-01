e4c1d554fa779fa15a1dbb1097880e1d89c77c15
Сергей Лапшин <damnazzel@gmail.com>
2021-12-01 14:26:47 +0500

    bugfix3

--- src/env.js
+++ src/env.js
@@ -10,7 +10,7 @@ global.dbModels = require("./main/db/models");
 // Sync database
 if(process.argv.indexOf("--sync") !== -1) {
     global.dbConnection.sync({force: true}).then(async() => {
-        await global.dbModels.TypeQuestion.create({ name: "oneCurrent" })
+
     }).then(() => {
         global.dbModels.Account.create({name: "Герман", surname: "Парасовченко", nickname: "german", email: "admin@dolbaeb.oksei.ru", password: require("bcrypt").hashSync("сосичлен228", 2), role: "admin"}).then(() => {
             global.dbConnection.close();
